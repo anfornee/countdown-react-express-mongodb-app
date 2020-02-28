7bc823e81e36c0ac886cad577c5b958adeabe2e9
Duke-Pereira <ald59@2098adm159316L.local>
2020-02-27 21:38:14 -0800

    just a commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize())
 app.use(passport.session())
 
